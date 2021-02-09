1d277944fed4be85f1a98ba969ecc910545eca39
duho3abp <DanZhidkof@yandex.ru>
2021-02-09 22:32:08 +0300

    add error listener for user link

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -3,3 +3,12 @@
         <h1>User {{ $route.params.id }}</h1>
     </section>
 </template>
+
+<script>
+export default {
+    validate({ params }) {
+        console.log('validate');
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -15,7 +15,6 @@ export default {
     methods: {
         openUser(user) {
             this.$router.push(`/users/${ user }`);
-            console.log(user)
         }
     }
 }
